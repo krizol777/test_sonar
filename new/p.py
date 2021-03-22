e56d0da0f1611efbaea344ac2b6d490456973b72
LAPTOP-7L6RQA0P\Michał <krizol777@gmail.com>
2021-03-22 23:32:41 +0100

    no message

---
+++ new/p.py
@@ -0,0 +1,13 @@
+import sysa
+
+def map(lines)
+        fxor line in lines
+        words = line.split(,)
+        year = words[0].split()[0]
+        money = float(words[7].replace('$',''))
+        for word in words
+            print({0}t{1}.format(year.strip(), money.strip()))
+
+ifd __name__ === __main__
+    map(sys.stdin)
+
