f49b279936ebf3954b784649aea7e7c9507eeeb0
LAPTOP-7L6RQA0P\Michał <krizol777@gmail.com>
2021-03-22 22:36:43 +0100

    no message

---
+++ p.py
@@ -0,0 +1,15 @@
+#!usrbinenv python
+
+import sys
+
+def map(lines)
+    for line in lines
+        words = line.split(,)
+        year = words[0].split()[0]
+        money = float(words[7].replace('$',''))
+        for word in words
+            print({0}t{1}.format(year.strip(), money.strip()))
+
+if __name__ == __main__
+    map(sys.stdin)
+
